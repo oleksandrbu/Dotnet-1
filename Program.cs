6b97fee5c44a2f0d75c0d18f74ec7675b6e84df7
Oleksandr Bukovskyi <oleksandrbu@tahometer.com>
2020-08-04 12:09:51 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Dotnet_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start, end;
+            DateTime[] holidays = {new DateTime(2020, 8, 24), new DateTime(2020, 8, 5)};
+
+            start = new DateTime(2020, 6, 1);
+            end = new DateTime(2020, 8, 30);
+
+            CountWorkingDays(start, end, holidays);
+        }
+        public static void CountWorkingDays(DateTime start, DateTime end, DateTime[] holidays)
+        {
+            TimeSpan dif = end.Subtract(start);
+            double count  = dif.TotalDays;
+
+            for(int i = 0; i < holidays.Length; i++){
+                if (holidays[i].CompareTo(start) >= 0 && holidays[i].CompareTo(end) <= 0){
+                    count--;
+                }
+            }
+            Console.Write(count);
+        }
+    }
+}
